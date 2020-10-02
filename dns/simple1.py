a95192b4dab6bb74945fa5d25adaaf67cb475c65
Knowing-feng <68280177+Knowing-feng@users.noreply.github.com>
2020-10-02 15:30:23 +0800

    需要改成交互脚本

--- dns/simple1.py
+++ dns/simple1.py
@@ -1,7 +1,42 @@
 import dns.resolver
 
-domain = input('Please input an domain: ')      # 输入域名地址
-A = dns.resolver.resolve(domain, 'A')             # 指定查询类型为A记录
-for i in A.response.answer:
-    for j in i.items:
-        print(j.address)
+
+class DNSDetect(object):
+    def __init__(self, domain):
+        self.domain = domain
+
+    def a(self):
+        A = dns.resolver.resolve(self.domain, 'A')             # 指定查询类型为A记录
+        for i in A.response.answer:
+            for j in i.items:
+                print(j.address)
+
+    def mx(self):
+        try:
+            MX = dns.resolver.resolve(self.domain, 'MX')
+        except dns.resolver.NoAnswer:
+            print(f'{self.domain}没有MX记录')
+            return False
+        for i in MX:
+            print(i)
+        return True
+
+    def ns(self):
+        """只限一级域名"""
+        ns = dns.resolver.resolve(self.domain, 'NS')
+        for i in ns.response.answer:
+            for j in i.items:
+                print(j.to_text())
+
+    def cname(self):
+        cname = dns.resolver.resolve(self.domain, 'CNAME')
+        for i in cname.response.answer:
+            for j in i.items:
+                print(j.to_text())
+
+
+
+if __name__ == '__main__':
+    domain = input('Please input an domain: ')
+    obj = DNSDetect(domain)
+    obj.cname()
